6e8b8d44cdb693a3225edcdbea58b181c527d72f
Nikita Mozharovskii <90044616+NMozharovskii@users.noreply.github.com>
2021-11-24 09:58:55 +0300

    Multithreaded Server

---
+++ MultithreadedServer/client2.py
@@ -0,0 +1,13 @@
+import socket
+
+sock = socket.socket()
+sock.setblocking(1)
+sock.connect(('localhost', 7777)) #Подключаемся
+
+msg = input()
+#msg = "Hi!"
+sock.send(msg.encode()) #Отправка сообщения
+data = sock.recv(1024) #Количество байт
+sock.close() #Закрываем
+
+print(data.decode())
